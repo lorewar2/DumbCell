380f99331a971662cc61d1d251a68af45af1f1f4
lorewar2 <mw8511@live.com>
2024-02-13 04:55:38 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,52 @@
+import random
+import numpy as np
+
+def get_softmax(raw_probs):
+    softmax_probs = np.array(raw_probs)
+    for i in range(len(softmax_probs)):
+        softmax_probs[i] = np.exp(softmax_probs[i])
+    sum = np.sum(softmax_probs)
+    for i in range(len(softmax_probs)):
+        softmax_probs[i] = softmax_probs[i] / sum
+    return softmax_probs
+
+def generate_test_set(num_of_variant_loc, depth, cell_num):
+
+    return
+
+def expectation_step():
+    return
+
+def maximization_step():
+    return
+
+def visualize():
+    # give random y to visualize better in 2d i guess?
+
+    return
+
+def main():
+    # seed this
+    random.seed(3)
+    np.random.seed(2)
+    # variables 
+    variant_num = 20
+    cluster_num = 2
+    cell_num = 10
+    cluster_probs = np.empty((variant_num, cluster_num))
+    # clsuter prob array initialize randomly
+    for i in range(variant_num):
+        random_probs = [random.random() for i in range(cluster_num)]
+        softmax_probs = get_softmax(random_probs)
+        cluster_probs[i] = softmax_probs
+    #print(cluster_probs)
+    # make a test set
+
+    return
+
+
+
+
+
+if __name__ == "__main__":
+    main()
